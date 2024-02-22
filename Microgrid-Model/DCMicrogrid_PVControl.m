ed421a93b836a3544657f11f1fec786886d214b6
Andrew Boland <65364090+agboland@users.noreply.github.com>
2024-02-22 10:31:13 -0500

    Add files via upload

--- Microgrid-Model/DCMicrogrid_PVControl.m
+++ Microgrid-Model/DCMicrogrid_PVControl.m
@@ -7,14 +7,21 @@ time_hours = (time / 100) * 24;
 % Define irradiance array
 irradiance = zeros(size(time));
 for i = 1:length(time)
-    if time_hours(i) >= 6 && time_hours(i) < 18  % Daytime from 6 AM to 6 PM
-        irradiance(i) = 1000 * sin(pi * (time_hours(i) - 6) / 12); % Peak at 3 PM
+    if time_hours(i) >= 6 && time_hours(i) < 18  % Daytime from 6 AM to 8 PM
+        if time_hours(i) <= 15  % Before 3 PM
+            % Adjusted slope for a lower rise to peak irradiance
+            irradiance(i) = 1000 * (1 - cos(pi * (time_hours(i) - 6) / 9)) / 2;
+        else  % After 3 PM
+            % Steeper drop-off after peak irradiance
+            irradiance(i) = 1000 * (1 + cos(pi * (time_hours(i) - 15) / 3)) / 2;
+        end
     end
 end
 
+
 % Define temperature array
 % Adjusting the phase to make the peak around the 60-second mark
-temperature = 14.4444 + (25.5556 - 14.4444) * sin(2 * pi * (time / 100) - (2 * pi * 35 / 100));
+temperature = 14.4444 + (25.5556 - 14.4444) * sin(2 * pi * (time / 100) - (2 * pi * 30 / 100));
 
 % Create timeseries objects
 ts_irradiance = timeseries(irradiance, time);
